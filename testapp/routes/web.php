cdad6c82fe9e56a9a31d253649f82fe4da4e7fcb
chivasxian <chivasxian@gmail.com>
2025-09-26 14:05:07 +0800

    test name routes

--- testapp/routes/web.php
+++ testapp/routes/web.php
@@ -4,4 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    //return '<h1>Hello, World!</h1>';
 });
+
+route::prefix('/admin')->name('admin.')->group(function () {
+    route::prefix('/users')->group(function () {
+        route::get('/profile', function () {
+        return 'User Profile';
+    })->name('profile');
+        Route::get('/information', function () {
+            return 'User Information';
+        });
+    });
+
+});
+
