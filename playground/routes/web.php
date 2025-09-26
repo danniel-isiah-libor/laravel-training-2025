c2852a026584f0dae27a5f071f0c8d03ecc63291
danniel-isiah-libor <dlibor.dev@gmail.com>
2025-09-26 14:05:04 +0800

    Day 1: routes

--- playground/routes/web.php
+++ playground/routes/web.php
@@ -4,4 +4,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return '<h1>hello world</h1>';
 });
+
+Route::prefix('/admin')->name('admin.')->group(function () {
+    Route::prefix('/users')->group(function () {
+        Route::get('/profile', function () {
+            return 'User Profile';
+        })->name('profile');
+
+        Route::get('/information', function () {
+            return 'User Information';
+        })->name('information');
+    });
+
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    })->name('dashboard');
+});
+
+Route::get('/go-to-admin', function () {
+    // logic here.....
+    return redirect()->route('admin.dashboard');
+});
+
+// Route::redirect('/go-to-admin', '/admin/dashboard');
