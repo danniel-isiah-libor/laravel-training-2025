f730a95c6891504c944cecd5f6a48dd44e29a308
lance-pallesco <lc.pallesco@gmail.com>
2025-09-26 14:15:14 +0800

    route testing

--- training-example/routes/web.php
+++ training-example/routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', function () {
+    return 'User Profile';
+})->name('users');
+
+Route::prefix('/admin')->name('admin.')->group(function () {
+    Route::get('/profile', function () {
+        return 'Admin Profile Display';
+    })->name('profile');
+
+    Route::get('/event-logs', function () {
+        return 'Admin Event Logs';
+    })->name('event-logs');
+});
