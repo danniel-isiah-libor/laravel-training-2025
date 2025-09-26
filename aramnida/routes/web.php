4eff8d0f293f19bad1cd6ab6e585db4d2f9951e9
hersonzosa <hersonzosa@gmail.com>
2025-09-26 14:04:04 +0800

    route testing

--- aramnida/routes/web.php
+++ aramnida/routes/web.php
@@ -4,4 +4,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+        // return '<h1>Hello Ice!</h1>';
 });
+
+
+// Route::prefix('/admin')->group(function (){
+//     Route::prefix('/users')->group(function (){
+//         Route::get('profile', function () {
+//         // Matches The "/admin/users" URL
+//         return 'User profile';
+//         });
+//     });
+// });
+
+Route::prefix('/admin')->name('admin.')->group(function (){
+    Route::prefix('/users')->group(function (){
+        Route::get('/profile', function () {
+            return 'User profile';
+        })->name('profile');
+
+        Route::get('/information/fgfgfgfgfg/hhghghhg', function () {
+            return 'User Information';
+        })->name('information');
+
+        Route::get('/dashboard', function() {
+            return 'Admin Dashboard';
+        })->name('dashboard');
+    });
+});
+
+Route::redirect('/go-to-admin', '/admin/users/dashboard');
