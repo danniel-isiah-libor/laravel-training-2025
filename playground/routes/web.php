e17644d1ca2000b172867b34974a4bf39c20c034
aelocarinasCAB <locarinasangelinecab@gmail.com>
2025-09-26 14:02:58 +0800

    feat(route): add routes

--- playground/routes/web.php
+++ playground/routes/web.php
@@ -3,5 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "<html>
+       <marquee> INAANTOK KA NA TIN? </marquee>
+    </html>";
+});
+
+Route::prefix('/admin')->name('admin.')->group(function (){
+    Route::prefix('/users')->group(function(){
+
+        Route::get('/profile', function(){
+            return 'User Profile';
+        })->name('profile');
+
+        Route::get('/users', function(){
+            return 'Admin User';
+        })->name('users');
+
+        Route::get('/information', function(){
+            return 'Admin Information';
+        })->name('information');
+        
+    });
+    
 });
