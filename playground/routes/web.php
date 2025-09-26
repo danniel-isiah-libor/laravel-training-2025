89200fd693416e249ae98ba98f2f32a12900251b
ChristineRomasanta <christine.romasantacab@gmail.com>
2025-09-26 14:05:24 +0800

    Day 1: routes

--- playground/routes/web.php
+++ playground/routes/web.php
@@ -4,4 +4,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+    // return ' ';
 });
+
+Route::prefix('/admin')->name('admin.')->group(function () {
+    Route::prefix('/users') -> group (function () {
+        Route::get('/profile', function () {
+            return 'User Profile';
+        })->name('userProfile');
+    });
+
+    Route::get('/information', function () {
+        return 'User Information Profile';
+    })->name('userInformation');
+});
+
+Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    })->name('admin');
+
+
+Route::get('/go-to-admin', function () {
+    return redirect()->route('admin.userProfile');
+});
+
+
+
+
