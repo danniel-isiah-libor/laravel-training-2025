d9211c8d0188d73f43527ce65226eb47aba89d65
JiboCobar <cobar.jesusemmanueljr@gmail.com>
2025-09-26 14:06:47 +0800

    Activity One

--- cobar_laravel_exercise/routes/web.php
+++ cobar_laravel_exercise/routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/admin')->name('admin.')->group(function(){
+    Route::prefix('/users')->name('users.')->group(function(){
+        Route::get('/profile', function(){
+            return 'View Profile';
+        })->name('profile');
+
+        Route::get('information', function(){
+            return 'View Information';
+        })->name('information');
+    });
+
+    Route::get('/dashboard', function(){
+        return 'Admin Dashboard';
+    })->name('dashboard');
+});
+
+// Route::redirect('/go-to-admin', '/admin/dashboard', 301);
+
+Route::get('go-to-admin', function(){
+    return redirect()->route('admin.dashboard');
+});
