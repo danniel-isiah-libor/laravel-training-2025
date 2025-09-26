5acab3d0249f2265a0824c58d13da2b7aec73763
bomacs <eymardfaderon@gmail.com>
2025-09-26 14:08:05 +0800

    feat(route): add routes

--- test_app/routes/web.php
+++ test_app/routes/web.php
@@ -4,4 +4,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return '<h1>Hello World</h1>';
+});
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::prefix('users')->group(function() {
+        Route::get('/profile', function () {
+            return 'User Profile'; 
+        })->name('profile');
+
+        Route::get('/information', function () {
+            return 'User INformation';
+        })->name('information');
+    });
+
+    Route::get('/dashboard', function () {
+        return '<h1>This is dashboard page.</h1>';
+    });
+
+    Route::get('/go-to-admin', function () {
+        return redirect()->route('admin.profile');
+    });
 });
