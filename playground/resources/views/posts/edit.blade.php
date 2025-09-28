0e96185c23b0e19fa410311f54c15882e5893a42
danniel-isiah-libor <dlibor.dev@gmail.com>
2025-09-28 17:42:33 +0800

    feat: Add delete functionality for posts in PostController and update edit view

--- playground/resources/views/posts/edit.blade.php
+++ playground/resources/views/posts/edit.blade.php
@@ -1,3 +1,12 @@
 <x-layout>
+    {{-- <a href="{{ route('posts.destroy', $post) }}">Delete Post</a> --}}
+
+    <form action="{{ route('posts.destroy', $post) }}" method="POST">
+        @csrf
+        @method('DELETE')
+
+        <x-forms.button label="Delete Post"/>
+    </form>
+
     <x-forms.post action="{{ route('posts.update', $post) }}" label="Edit Post" method="PUT" :data="$post"/>
 </x-layout>
